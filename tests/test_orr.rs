15fa8b8f7111dd34b24e6b843b259d47844870c5
johannes borg <johannes.borg@hotmail.com>
2021-01-18 17:34:23 +0100

    Refactor all tests to use new flag assert macro
    All tests now assert value of all 4 condition flags, so the tests are broader

--- tests/test_orr.rs
+++ tests/test_orr.rs
@@ -13,12 +13,10 @@ Included cases:
 - Set Zero flag if result is zero
 - Set Negative flag if result is negative
 
-Checks include in all cases: 
-- Never set Carry or V (signed overflow) flags
-- Never alter untargeted low registers
-TODO: Check that C or V in never *unset*?
+All tests also check for unexpected changes in untargeted lo registers and condition flags
+TODO: Add tests where untargeted values are pre-set and check if incorrectly reset?
 
-The reference for these tests are currently official documentations and a QEMU-based VM
+The reference for these tests is currently official documentations and a QEMU-based VM
 TODO: Test against a hardware Cortex-M0 to make sure it's actually up to spec?
 
 */
@@ -30,8 +28,7 @@ pub fn test_orr_register(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm, 0xFF, 0xAA);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, false, false, false, false);
 }
 
 // Test if OR(0, 0) correctly sets ZERO flag
@@ -41,9 +38,7 @@ pub fn test_orr_flag_zero(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm);
-    assert!(vm.cpsr.z);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, false, true, false, false);
 }
 
 // Test if OR(1000 ... 0000, 1000 ... 0000) correctly sets NEGATIVE flag
@@ -54,7 +49,5 @@ pub fn test_orr_flag_neg(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm, 0x8000_0000);
-    assert!(vm.cpsr.n);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, true, false, false, false);
 }
--- tests/test_tst.rs
+++ tests/test_tst.rs
@@ -7,18 +7,16 @@ use common::*;
 
 Unit test for TST operator - bitwise AND that only set flags and discard result
 
-Included cases (same as for AND): 
+Included cases: 
 
 - Don't alter target register (Unlike AND)
 - Set Zero flag if result is zero
 - Set Negative flag if result is negative
 
-Checks include in all cases: 
-- Never set Carry or V (signed overflow) flags
-- Never alter untargeted low registers
-TODO: Check that C or V in never *unset*?
+All tests also check for unexpected changes in untargeted lo registers and condition flags
+TODO: Add tests where untargeted values are pre-set and check if incorrectly reset?
 
-The reference for these tests are currently official documentations and a QEMU-based VM
+The reference for these tests is currently official documentations and a QEMU-based VM
 TODO: Test against a hardware Cortex-M0 to make sure it's actually up to spec?
 
 */
@@ -30,8 +28,7 @@ pub fn test_tst_unaltered(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm, 0xF, 0x1);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, false, false, false, false);
 }
 
 // Test if TST(0, 0) correctly sets ZERO flag
@@ -41,9 +38,7 @@ pub fn test_tst_flag_zero(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm, 0x5, 0xA);
-    assert!(vm.cpsr.z);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, false, true, false, false);
 }
 
 // Test if TST(1000 ... 0000, 1000 ... 0000) correctly sets NEGATIVE flag
@@ -54,7 +49,5 @@ pub fn test_tst_flag_neg(){
     vm.execute().unwrap();
     vm.print_diagnostics();
     assert_lo_regs!(vm, 0x8000_0000);
-    assert!(vm.cpsr.n);
-    assert!(!vm.cpsr.c);
-    assert!(!vm.cpsr.v);
+    assert_flags_nzcv!(vm, true, false, false, false);
 }
