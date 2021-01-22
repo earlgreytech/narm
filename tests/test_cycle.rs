ff03f52a05b41f7b2a0175d8936db31021440606
dezuman <johannes.borg@hotmail.com>
2021-01-22 21:29:52 +0100

    Apply rustfmt formatting to files

--- tests/test_cycle.rs
+++ tests/test_cycle.rs
@@ -6,31 +6,34 @@ use common::*;
 // this file is for basic smoke testing of the actual common.rs test functions
 
 #[test]
-pub fn test_cycle(){
-    let mut vm = create_vm_from_asm("
+pub fn test_cycle() {
+    let mut vm = create_vm_from_asm(
+        "
         movs r0,            #0xF1
-    ");
+    ",
+    );
     vm.cycle().unwrap();
     vm.print_diagnostics();
     let mut vm_expected: VMState = Default::default();
-    
+
     vm_expected.r[0] = Some(0xF1);
-    
+
     assert_vm_eq!(vm_expected, vm);
 }
 
 #[test]
-pub fn test_execute(){
-    let mut vm = create_vm_from_asm("
+pub fn test_execute() {
+    let mut vm = create_vm_from_asm(
+        "
         movs r0,            #0xF1
         svc                 #0xFF
-    ");
+    ",
+    );
     assert_eq!(vm.execute().unwrap(), 0xFF);
     vm.print_diagnostics();
     let mut vm_expected: VMState = Default::default();
-    
+
     vm_expected.r[0] = Some(0xF1);
-    
+
     assert_vm_eq!(vm_expected, vm);
 }
-
--- tests/test_orr.rs
+++ tests/test_orr.rs
@@ -7,7 +7,7 @@ use common::*;
 
 Unit test for bitwise OR operator
 
-Included cases: 
+Included cases:
 
 - Calculate correctly for two register values
 - Set Zero flag if result is zero
@@ -23,56 +23,62 @@ TODO: Test against a hardware Cortex-M0 to make sure it's actually up to spec?
 
 // Test if operation calculate correctly for two register values
 #[test]
-pub fn test_orr_register(){
-    let mut vm = create_vm_from_asm("
+pub fn test_orr_register() {
+    let mut vm = create_vm_from_asm(
+        "
         movs r0,            #0x05
         movs r1,            #0xAA
         orrs r0, r1
         svc                 #0xFF
-    ");
+    ",
+    );
     assert_eq!(vm.execute().unwrap(), 0xFF);
     vm.print_diagnostics();
     let mut vm_expected: VMState = Default::default();
-    
+
     vm_expected.r[0] = Some(0xAF);
     vm_expected.r[1] = Some(0xAA);
-    
+
     assert_vm_eq!(vm_expected, vm);
 }
 
 // Test if operation set Zero flag if result is zero
 #[test]
-pub fn test_orr_flag_zero(){
-    let mut vm = create_vm_from_asm("
+pub fn test_orr_flag_zero() {
+    let mut vm = create_vm_from_asm(
+        "
         orrs r0, r1
         svc                 #0xFF
-    ");
+    ",
+    );
     assert_eq!(vm.execute().unwrap(), 0xFF);
     vm.print_diagnostics();
     let mut vm_expected: VMState = Default::default();
-    
+
     vm_expected.z = Some(true);
-    
+
     assert_vm_eq!(vm_expected, vm);
 }
 
 // Test if operation set Negative flag if result is negative
 // ("highest" bit indicate sign in int representation, so setting it to 1 -> negative number)
 #[test]
-pub fn test_orr_flag_neg(){
-    let mut vm = create_vm_from_asm("
+pub fn test_orr_flag_neg() {
+    let mut vm = create_vm_from_asm(
+        "
         movs r0,            #0x02
         lsls r0,            #0x0F
         lsls r0,            #0x0F
         orrs r0, r0
         svc                 #0xFF
-    ");
+    ",
+    );
     assert_eq!(vm.execute().unwrap(), 0xFF);
     vm.print_diagnostics();
     let mut vm_expected: VMState = Default::default();
-    
+
     vm_expected.r[0] = Some(0x8000_0000);
     vm_expected.n = Some(true);
-    
+
     assert_vm_eq!(vm_expected, vm);
 }
