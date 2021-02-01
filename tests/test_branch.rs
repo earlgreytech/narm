056eb11276e2b89c0de66a13c70ee11ea9ae9f5b
dezuman <johannes.borg@hotmail.com>
2021-02-01 01:47:24 +0100

    Finalize testing for branch operators

--- tests/test_branch.rs
+++ tests/test_branch.rs
@@ -11,20 +11,23 @@ Unit test for Branch operators
 Included varieties:
 
 B <label> T2            - Branch unconditionally by label, jump size restricted to signed 11 bits
-B<cond> <label> T1      - Branch conditionally by label, jump size restricted to signed 8 bits
+B<C> <label> T1         - Branch conditionally by label, jump size restricted to signed 8 bits
 BX <Rm> T1              - Branch by register
-BL <label T1            - Branch by label, set link register, jump size restricted to even signed 25 bits (?????)
+BL <label> T1           - Branch by label, set link register, jump size restricted to even signed 25 bits (?????)
 BLX <Rm> T1             - Branch by register, set link register
 
 General test cases:
 
 - Branch forward
-- Branch far forward (Causing memory error)
 - Branch backward
+- Branch far forward (Causing memory error)
 - Branch far backward (Causing memory error)
-- Call, and return from, subroutine using link register
 
-Special test case for B<cond>
+Special test case for BLX <Rm> T1:
+
+- Branch and then branch back using address saved in link register
+
+Special test case for B<C>:
 
 - Test all different conditions
 
@@ -36,9 +39,9 @@ TODO: Test against a hardware Cortex-M0 to make sure it's actually up to spec?
 // String representation of ops for use in debug output
 const OPCODES: &'static [&'static str] = &[
     "B <label> T2",
-    "B<cond> <label> T1",
+    "B<C> <label> T1",
     "BX <Rm> T1",
-    "BL <label T1 32bit",
+    "BL <label> T1 32bit",
     "BLX <Rm> T1",
 ];
 
@@ -55,7 +58,6 @@ pub fn test_branch_forward() {
     let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
 
     // Tell macros which op varieties are tested in this function
-    //let applicable_op_ids = vec![0, 1, 2, 3, 4];
     let applicable_op_ids = vec![0, 1, 2, 3, 4];
 
     // Common pre-execution state
@@ -76,7 +78,7 @@ pub fn test_branch_forward() {
     let ops0 = format!("b test1 {}", post_ops);
     create_vm!(vms, vm_states, 0, code_var = true, ops0);
 
-    // 1: B<cond> <label> T1 (BNE -> if flag Z = 0)
+    // 1: B<C> <label> T1 (BNE -> if flag Z = 0)
     let ops1 = format!("bne test1 {}", post_ops);
     create_vm!(vms, vm_states, 1, code_var = true, ops1);
 
@@ -84,15 +86,15 @@ pub fn test_branch_forward() {
     let ops2 = format!("bx r1 {}", post_ops);
     create_vm!(vms, vm_states, 2, code_var = true, ops2);
 
-    // 3: BL <label T1 32bit
+    // 3: BL <label> T1 32bit
     let ops3 = format!("bl test1 {}", post_ops);
     create_vm!(vms, vm_states, 3, code_var = true, ops3);
-    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE);
+    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE); // Location of the BL instruction is loaded to link reg
 
     // 4: BLX <Rm> T1
     let ops4 = format!("blx r1 {}", post_ops);
     create_vm!(vms, vm_states, 4, code_var = true, ops4);
-    vm_states[4].r[14] = Some(ASM_ENTRY + OP_SIZE + THUMBS_MODE);
+    vm_states[4].r[14] = Some(ASM_ENTRY + OP_SIZE + THUMBS_MODE); // Location of the BLX instruction is loaded to link reg
 
     // Common expected post-execution state
     common_state!(applicable_op_ids, vm_states.r[0] = Some(0xCD));
@@ -100,91 +102,314 @@ pub fn test_branch_forward() {
     run_test!(vms, vm_states, applicable_op_ids);
 }
 
-//this will test small nearby jumps using bl
+// Branch backward
 #[test]
-pub fn test_bl_small_jump() {
-    println!("\n>>> TODOOOOOT \n");
+pub fn test_branch_backward() {
+    println!("\n>>> Branch ops test case: Branch backward \n");
 
     // Arrays holding instances of VMs and matching state structs
     let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
     let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
 
     // Tell macros which op varieties are tested in this function
-    let applicable_op_ids = vec![3];
+    let applicable_op_ids = vec![0, 1, 2, 3, 4];
+
+    // Common pre-execution state
+    common_state!(
+        applicable_op_ids,
+        vm_states.r[1] = Some(ASM_ENTRY + THUMBS_MODE)
+    );
+    common_state!(
+        applicable_op_ids,
+        vm_states.pc_address = Some(ASM_ENTRY + 2 * OP_SIZE + THUMBS_MODE)
+    );
+
+    // VM initialization
+    let pre_ops = "
+        test1:
+        movs r0,        #0xCD
+        svc             #0xFF
+        ";
+
+    let post_ops = "
+        movs r0,        #0x00
+        svc             #0x01
+        ";
+
+    // 0: B <label> T2"
+    let ops0 = format!("{}b test1 {}", pre_ops, post_ops);
+    create_vm!(vms, vm_states, 0, code_var = true, ops0);
+
+    // 1: B<C> <label> T1 (BNE -> if flag Z = 0)
+    let ops1 = format!("{}bne test1 {}", pre_ops, post_ops);
+    create_vm!(vms, vm_states, 1, code_var = true, ops1);
+
+    // 2: BX <Rm> T1
+    let ops2 = format!("{}bx r1 {}", pre_ops, post_ops);
+    create_vm!(vms, vm_states, 2, code_var = true, ops2);
+
+    // 3: BL <label> T1 32bit
+    let ops3 = format!("{}bl test1 {}", pre_ops, post_ops);
+    create_vm!(vms, vm_states, 3, code_var = true, ops3);
+    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + OP_SIZE * 2 + THUMBS_MODE); // Location of the BL instruction is loaded to link reg
+
+    // 4: BLX <Rm> T1
+    let ops4 = format!("{}blx r1 {}", pre_ops, post_ops);
+    create_vm!(vms, vm_states, 4, code_var = true, ops4);
+    vm_states[4].r[14] = Some(ASM_ENTRY + 3 * OP_SIZE + THUMBS_MODE); // Location of the BLX instruction is loaded to link reg
+
+    // Common expected post-execution state
+    common_state!(applicable_op_ids, vm_states.r[0] = Some(0xCD));
+    common_state!(applicable_op_ids, vm_states.pc_address = None);
+
+    run_test!(vms, vm_states, applicable_op_ids);
+}
+
+// Branch far forward (Causing memory error)
+#[test]
+pub fn test_branch_far_forward() {
+    println!("\n>>> Branch ops test case: Branch far forward (Causing memory error) \n");
+
+    // Arrays holding instances of VMs and matching state structs
+    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
+    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+
+    // Tell macros which op varieties are tested in this function
+    let applicable_op_ids = vec![2, 3, 4];
+
+    // Common pre-execution state
+    common_state!(
+        applicable_op_ids,
+        vm_states.r[1] = Some(ASM_ENTRY + 100000 * OP_SIZE + THUMBS_MODE) // 0x
+    );
+    common_state!(applicable_op_ids, vm_states.expect_exec_error = true);
+
+    // VM initialization
+
+    // 0: B <label> T2" - Not applicable
+
+    // 1: B<C> <label> T1  - Not applicable
+
+    // 2: BX <Rm> T1
+    create_vm!(vms, vm_states, 2, "bx r1");
+
+    // 3: BL <label> T1 32bit
+    create_vm!(vms, vm_states, 3, "bl #0xF0001");
+    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE); // Location of the BL instruction is loaded to link reg
+
+    // 4: BLX <Rm> T1
+    create_vm!(vms, vm_states, 4, "blx r1");
+    vm_states[4].r[14] = Some(ASM_ENTRY + OP_SIZE + THUMBS_MODE); // Location of the BLX instruction is loaded to link reg
+
+    run_test!(vms, vm_states, applicable_op_ids);
+}
+
+// Branch far backward (Causing memory error)
+#[test]
+pub fn test_branch_far_backward() {
+    println!("\n>>> Branch ops test case: Branch far backward (Causing memory error) \n");
+
+    // Arrays holding instances of VMs and matching state structs
+    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
+    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+
+    // Tell macros which op varieties are tested in this function
+    let applicable_op_ids = vec![2, 3, 4];
+
+    // Common pre-execution state
+    common_state!(applicable_op_ids, vm_states.r[1] = Some(0x50));
+    common_state!(applicable_op_ids, vm_states.expect_exec_error = true);
+
+    // VM initialization
+
+    // 0: B <label> T2" - Not applicable
+
+    // 1: B<C> <label> T1  - Not applicable
+
+    // 2: BX <Rm> T1
+    create_vm!(vms, vm_states, 2, "bx r1");
+
+    // 3: BL <label> T1 32bit
+    create_vm!(vms, vm_states, 3, "bl #0x50");
+    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE); // Location of the BL instruction is loaded to link reg
+
+    // 4: BLX <Rm> T1
+    create_vm!(vms, vm_states, 4, "blx r1");
+    vm_states[4].r[14] = Some(ASM_ENTRY + OP_SIZE + THUMBS_MODE); // Location of the BLX instruction is loaded to link reg
+
+    run_test!(vms, vm_states, applicable_op_ids);
+}
+
+// Branch and then branch back using address saved in link register
+#[test]
+pub fn test_branch_and_return() {
+    println!("\n>>> BLX <Rm> T1 special test case: Branch and then branch back using address saved in link register \n");
+
+    // Arrays holding instances of VMs and matching state structs
+    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
+    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+
+    // Tell macros which op varieties are tested in this function
+    let applicable_op_ids = vec![4];
+
+    // 4: BLX <Rm> T1
+    vm_states[4].r[1] = Some(ASM_ENTRY + 2 * OP_SIZE + THUMBS_MODE);
     create_vm!(
         vms,
         vm_states,
-        3,
+        4,
         multiline = true,
         "
-        bl test1
+        blx r1
+        svc             #0xFF
+        movs r0,        #0xCD
+        blx r14
+        movs r0,        #0x01
+        svc             #0x01
+    "
+    );
+    vm_states[4].r[14] = Some(ASM_ENTRY + 3 * OP_SIZE + THUMBS_MODE); // Location of the last BLX instruction is loaded to link reg
+    vm_states[4].r[0] = Some(0xCD);
+
+    run_test!(vms, vm_states, applicable_op_ids);
+}
+
+// Test all different conditions
+#[test]
+pub fn test_branch_conditions() {
+    println!("\n>>> B<C> <label> T1 special test case: Test all different conditions \n");
+
+    // VM initialization
+    let post_ops = "
+        movs r0,        #0x01
         svc             #0x01
         test1:
-        movs r0,        #0x01
         svc             #0xFF
-    "
-    );
+        ";
 
-    vm_states[3].r[0] = Some(1);
-    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE);
+    println!("\n>>> Running tests for 15 condition types \n");
 
-    run_test!(vms, vm_states, applicable_op_ids);
-}
+    // EQ: Z == 1
+    println!("\n>>> [1/14] Testing for condition type: EQ \n");
+    let mut vm = create_vm_from_asm(&format!("beq test1 {}", post_ops));
+    vm.cpsr.z = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-#[test]
-pub fn test_bl_big_jump() {
-    //note this test will access unavailable memory
-    println!("\n>>> TODOOOOOT \n");
+    // NE: Z == 0
+    println!("\n>>> [2/14] Testing for condition type: NE \n");
+    let mut vm = create_vm_from_asm(&format!("bne test1 {}", post_ops));
+    vm.cpsr.z = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    // Arrays holding instances of VMs and matching state structs
-    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
-    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+    // CS: C == 1
+    println!("\n>>> [3/14] Testing for condition type: CS \n");
+    let mut vm = create_vm_from_asm(&format!("bcs test1 {}", post_ops));
+    vm.cpsr.c = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    // Tell macros which op varieties are tested in this function
-    let applicable_op_ids = vec![3];
-    create_vm!(
-        vms,
-        vm_states,
-        3,
-        multiline = true,
-        "
-        bl              #0xF0020
-        svc             #0x01
-    "
-    );
+    // CC: C == 0
+    println!("\n>>> [4/14] Testing for condition type: CC \n");
+    let mut vm = create_vm_from_asm(&format!("bcc test1 {}", post_ops));
+    vm.cpsr.c = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    vm_states[3].expect_exec_error = true;
-    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE);
-    vm_states[3].pc_address = Some(0xF_0020);
+    // MI: N == 1
+    println!("\n>>> [5/14] Testing for condition type: MI \n");
+    let mut vm = create_vm_from_asm(&format!("bmi test1 {}", post_ops));
+    vm.cpsr.n = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    run_test!(vms, vm_states, applicable_op_ids);
-}
+    // PL: N == 0
+    println!("\n>>> [6/14] Testing for condition type: PL \n");
+    let mut vm = create_vm_from_asm(&format!("bpl test1 {}", post_ops));
+    vm.cpsr.n = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-#[test]
-pub fn test_bl_backward_jump() {
-    //note this test will access unavailable memory
-    println!("\n>>> TODOOOOOT \n");
+    // VS: V == 1
+    println!("\n>>> [7/14] Testing for condition type: VS \n");
+    let mut vm = create_vm_from_asm(&format!("bvs test1 {}", post_ops));
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    // Arrays holding instances of VMs and matching state structs
-    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
-    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+    // VC: V == 0
+    println!("\n>>> [8/14] Testing for condition type: VC \n");
+    let mut vm = create_vm_from_asm(&format!("bvc test1 {}", post_ops));
+    vm.cpsr.v = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    // Tell macros which op varieties are tested in this function
-    let applicable_op_ids = vec![3];
-    create_vm!(
-        vms,
-        vm_states,
-        3,
-        multiline = true,
-        "
-        bl              #0x50
-        svc             #0x01
-    "
-    );
+    // HI: C == 1 AND Z == 0
+    println!("\n>>> [9/14] Testing for condition type: HI \n");
+    let mut vm = create_vm_from_asm(&format!("bhi test1 {}", post_ops));
+    vm.cpsr.c = true;
+    vm.cpsr.z = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    vm_states[3].expect_exec_error = true;
-    vm_states[3].r[14] = Some(ASM_ENTRY + OP_SIZE_32BIT + THUMBS_MODE);
-    vm_states[3].pc_address = Some(0x50);
+    // LS: C == 0 OR  Z == 1
+    println!("\n>>> [10/14] Testing for condition type: LS \n");
+    let mut vm = create_vm_from_asm(&format!("bls test1 {}", post_ops));
+    vm.cpsr.c = false;
+    vm.cpsr.z = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 
-    run_test!(vms, vm_states, applicable_op_ids);
+    let mut vm = create_vm_from_asm(&format!("bls test1 {}", post_ops));
+    vm.cpsr.c = true;
+    vm.cpsr.z = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    // GE: N == V
+    println!("\n>>> [11/14] Testing for condition type: GE \n");
+    let mut vm = create_vm_from_asm(&format!("bge test1 {}", post_ops));
+    vm.cpsr.n = true;
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    let mut vm = create_vm_from_asm(&format!("bge test1 {}", post_ops));
+    vm.cpsr.n = false;
+    vm.cpsr.v = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    // LT: N != V
+    println!("\n>>> [12/14] Testing for condition type: LT \n");
+    let mut vm = create_vm_from_asm(&format!("blt test1 {}", post_ops));
+    vm.cpsr.n = true;
+    vm.cpsr.v = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+    let mut vm = create_vm_from_asm(&format!("blt test1 {}", post_ops));
+    vm.cpsr.n = false;
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    // GT: Z == 0 AND N == V
+    println!("\n>>> [13/14] Testing for condition type: GT \n");
+    let mut vm = create_vm_from_asm(&format!("bgt test1 {}", post_ops));
+    vm.cpsr.z = false;
+    vm.cpsr.n = false;
+    vm.cpsr.v = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    let mut vm = create_vm_from_asm(&format!("bgt test1 {}", post_ops));
+    vm.cpsr.z = false;
+    vm.cpsr.n = true;
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    // LE: Z == 1 OR  N != V
+    println!("\n>>> [14/14] Testing for condition type: LE \n");
+    let mut vm = create_vm_from_asm(&format!("ble test1 {}", post_ops));
+    vm.cpsr.z = true;
+    vm.cpsr.n = true;
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    let mut vm = create_vm_from_asm(&format!("ble test1 {}", post_ops));
+    vm.cpsr.z = false;
+    vm.cpsr.n = true;
+    vm.cpsr.v = false;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
+
+    let mut vm = create_vm_from_asm(&format!("ble test1 {}", post_ops));
+    vm.cpsr.z = false;
+    vm.cpsr.n = false;
+    vm.cpsr.v = true;
+    assert_eq!(vm.execute().unwrap(), 0xFF);
 }
