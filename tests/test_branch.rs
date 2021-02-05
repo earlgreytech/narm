46dec4520d5ddc691f0edc4137bfc1ea98ac02ea
dezuman <johannes.borg@hotmail.com>
2021-02-05 01:22:35 +0100

    Add test for BL op to test function with different code alignments

--- tests/test_branch.rs
+++ tests/test_branch.rs
@@ -13,7 +13,7 @@ Included varieties:
 B <label> T2            - Branch unconditionally by label, jump size restricted to signed 11 bits
 B<C> <label> T1         - Branch conditionally by label, jump size restricted to signed 8 bits
 BX <Rm> T1              - Branch by register
-BL <label> T1           - Branch by label, set link register, jump size restricted to even signed 25 bits (?????)
+BL <label> T1 (32-bit)  - Branch by label, set link register, jump size restricted to even signed 25 bits (?????)
 BLX <Rm> T1             - Branch by register, set link register
 
 General test cases:
@@ -27,9 +27,13 @@ Special test case for BLX <Rm> T1:
 
 - Branch and then branch back using address saved in link register
 
-Special test case for B<C>:
+Special test case for B<C> <label> T1:
 
 - Test all different conditions
+
+Special test case for BL <label> T1 (32-bit): 
+
+- Test proper function with different op alignment
 
 The reference for these tests is currently official documentations and a QEMU-based VM
 TODO: Test against a hardware Cortex-M0 to make sure it's actually up to spec?
@@ -41,7 +45,7 @@ const OPCODES: &'static [&'static str] = &[
     "B <label> T2",
     "B<C> <label> T1",
     "BX <Rm> T1",
-    "BL <label> T1 32bit",
+    "BL <label> T1 (32-bit)",
     "BLX <Rm> T1",
 ];
 
@@ -83,7 +87,7 @@ pub fn test_branch_forward() {
     let ops2 = format!("bx r1 {}", post_ops);
     create_vm!(arrays = (vms, vm_states), op_id = 2, asm_var = ops2);
 
-    // 3: BL <label> T1 32bit
+    // 3: BL <label> T1 (32-bit)
     let ops3 = format!("bl test1 {}", post_ops);
     create_vm!(arrays = (vms, vm_states), op_id = 3, asm_var = ops3);
     vm_states[3].r[14] = Some(code_mem_address(OP_SIZE_32BIT)); // Location of the BL instruction is loaded to link reg
@@ -139,7 +143,7 @@ pub fn test_branch_backward() {
     let ops2 = format!("{}bx r1 {}", pre_ops, post_ops);
     create_vm!(arrays = (vms, vm_states), op_id = 2, asm_var = ops2);
 
-    // 3: BL <label> T1 32bit
+    // 3: BL <label> T1 (32-bit)
     let ops3 = format!("{}bl test1 {}", pre_ops, post_ops);
     create_vm!(arrays = (vms, vm_states), op_id = 3, asm_var = ops3);
     vm_states[3].r[14] = Some(code_mem_address(OP_SIZE_32BIT + OP_SIZE * 2)); // Location of the BL instruction is loaded to link reg
@@ -185,7 +189,7 @@ pub fn test_branch_far_forward() {
         asm_literal_add_svc = "bx r1"
     );
 
-    // 3: BL <label> T1 32bit
+    // 3: BL <label> T1 (32-bit)
     create_vm!(
         arrays = (vms, vm_states),
         op_id = 3,
@@ -233,7 +237,7 @@ pub fn test_branch_far_backward() {
         asm_literal_add_svc = "bx r1"
     );
 
-    // 3: BL <label> T1 32bit
+    // 3: BL <label> T1 (32-bit)
     create_vm!(
         arrays = (vms, vm_states),
         op_id = 3,
@@ -422,4 +426,47 @@ pub fn test_branch_conditions() {
     vm.cpsr.n = false;
     vm.cpsr.v = true;
     assert_eq!(vm.execute().unwrap(), 0xFF);
+}
+
+// Test proper function with different op alignment
+// This is tested because BL (apparently?) doesn't follow standard for pc-relative address calculation unless op is aligned by 4 in memory
+#[test]
+pub fn test_branch_alignment() {
+    println!("\n>>> BL <label> T1 (32bit) special test case: Test proper function with different op alignment \n");
+
+    // Arrays holding instances of VMs and matching state structs
+    let mut vms: [NarmVM; *NUM_OPCODES] = Default::default();
+    let mut vm_states: [VMState; *NUM_OPCODES] = Default::default();
+
+    // Tell macros which op varieties are tested in this function
+    let applicable_op_ids = vec![3];
+
+    // VM initialization
+    // 3: BL <label> T1 (32-bit)
+    create_vm!(
+        vms,
+        vm_states,
+        3,
+        multiline = true,
+        "
+        bl label1
+        svc             #0x01
+        label1:
+        bl label2
+        svc             #0x02
+        svc             #0x03
+        label2:
+        bl label3
+        svc             #0x04
+        svc             #0x05
+        svc             #0x06
+        label3:
+        svc             #0xFF
+        svc             #0x07
+        svc             #0x08
+    "
+    );
+    
+    vm_states[3].r[14] = None; // Link registry is not important in this test
+    run_test!(vms, vm_states, applicable_op_ids);
 }
